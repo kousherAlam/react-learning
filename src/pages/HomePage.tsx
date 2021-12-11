cad04c01c1967124d790b7cdc305aa1e9746e7df
Kousher Alam <kousheralampranto@gmail.com>
2021-12-11 21:38:07 +0600

    updated

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const HomePage = () => {
     return <div className=''>
-        <h1>Home Page</h1>
+        <h1 className='text-4xl underline font-bold mb-3'>Home Page</h1>
     </div>
 }
